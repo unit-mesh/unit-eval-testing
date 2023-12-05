2e0af801dbda6f884eb59069cf4d2f790d2a1954
Phodal Huang <h@phodal.com>
2023-12-05 10:28:11 +0800

    chore: init basic job processor

--- src/main/java/cc/unitmesh/testng/infra/PackageManager.java
+++ src/main/java/cc/unitmesh/testng/infra/PackageManager.java
@@ -3,7 +3,8 @@ package cc.unitmesh.testng.infra;
 public class PackageManager {
     private static volatile PackageManager instance;
 
-    private PackageManager() {}
+    private PackageManager() {
+    }
 
     public static PackageManager getInstance() {
         if (instance == null) {
@@ -14,5 +15,37 @@ public class PackageManager {
             }
         }
         return instance;
+    }
+
+    public void install(String packageName) {
+        System.out.println("Installing package " + packageName);
+    }
+
+    public void uninstall(String packageName) {
+        System.out.println("Uninstalling package " + packageName);
+    }
+
+    public void update(String packageName) {
+        System.out.println("Updating package " + packageName);
+    }
+
+    public void upgrade(String packageName) {
+        System.out.println("Upgrading package " + packageName);
+    }
+
+    public void downgrade(String packageName) {
+        System.out.println("Downgrading package " + packageName);
+    }
+
+    public void reinstall(String packageName) {
+        System.out.println("Reinstalling package " + packageName);
+    }
+
+    public void purge(String packageName) {
+        System.out.println("Purging package " + packageName);
+    }
+
+    public void clean(String packageName) {
+        System.out.println("Cleaning package " + packageName);
     }
 }
